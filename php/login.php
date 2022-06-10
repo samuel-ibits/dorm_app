4e76c4204720ace4beeca8bdf81b72146ca0a586
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-10 09:39:34 +0100

    main

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<?php
+
+$u=$_POST['phone'];
+$p=$_POST['pass'];
+
+$postRequest = array(
+    'firstFieldData' => $u,
+    'secondFieldData' => $p
+);
+
+$cURLConnection = curl_init('https://api.dorm.com.ng/loginapi.php');
+curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+$apiResponse = curl_exec($cURLConnection);
+curl_close($cURLConnection);
+
+// $apiResponse - available data from the API request
+$jsonArrayResponse - json_decode($apiResponse);
+echo $jsonArrayResponse;
+?>
