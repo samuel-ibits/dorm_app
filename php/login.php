548eb32a4367762035daff23f101e683fb836692
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-10 12:09:20 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -12,8 +12,8 @@ $url = "https://api.dorm.com.ng/loginapi.php?phone".$u."&pass".$p;
 	$result = json_decode($response);
 	echo $result;
 	echo "<table>";
-	echo "<tr><td>Order ID:</td><td>$result->order_id</td></tr>";
-	echo "<tr><td>Amount:</td><td>$result->amount</td></tr>";
+	echo "<tr><td>Order ID:</td><td>$result->userid</td></tr>";
+	
 	echo "<tr><td>Response Code:</td><td>$result->response_code</td></tr>";
 	echo "<tr><td>Response Desc:</td><td>$result->response_desc</td></tr>";
 	echo "</table>";
