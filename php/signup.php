ab9a9d8619997296147ddc18db0c33dc74f9806d
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-28 16:41:26 +0100

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -1,28 +1,28 @@
 <?php 
-function isValidTelephoneNumber(string $telephone, int $minDigits = 9, int $maxDigits = 14): bool {
-    if (preg_match('/^[+][0-9]/', $telephone)) { //is the first character + followed by a digit
-        $count = 1;
-        $telephone = str_replace(['+'], '', $telephone, $count); //remove +
-    }
+// function isValidTelephoneNumber(string $telephone, int $minDigits = 9, int $maxDigits = 14): bool {
+//     if (preg_match('/^[+][0-9]/', $telephone)) { //is the first character + followed by a digit
+//         $count = 1;
+//         $telephone = str_replace(['+'], '', $telephone, $count); //remove +
+//     }
     
-    //remove white space, dots, hyphens and brackets
-    $telephone = str_replace([' ', '.', '-', '(', ')'], '', $telephone); 
+//     //remove white space, dots, hyphens and brackets
+//     $telephone = str_replace([' ', '.', '-', '(', ')'], '', $telephone); 
 
-    //are we left with digits only?
-    return isDigits($telephone, $minDigits, $maxDigits); 
-}
+//     //are we left with digits only?
+//     return isDigits($telephone, $minDigits, $maxDigits); 
+// }
 
-function normalizeTelephoneNumber(string $telephone): string {
-    //remove white space, dots, hyphens and brackets
-    $telephone = str_replace([' ', '.', '-', '(', ')'], '', $telephone);
-    return $telephone;
-}
+// function normalizeTelephoneNumber(string $telephone): string {
+//     //remove white space, dots, hyphens and brackets
+//     $telephone = str_replace([' ', '.', '-', '(', ')'], '', $telephone);
+//     return $telephone;
+// }
 
-$tel = '+9112 345 6789';
-if (isValidTelephoneNumber($tel)) {
-    //normalize telephone number if needed
-    echo normalizeTelephoneNumber($tel); //+91123456789
-}
+// $tel = '+9112 345 6789';
+// if (isValidTelephoneNumber($tel)) {
+//     //normalize telephone number if needed
+//     echo normalizeTelephoneNumber($tel); //+91123456789
+// }
 
 
 
