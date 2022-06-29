ec4504440b31998a501507b88a26c81c60ef927d
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-29 20:59:02 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -24,7 +24,7 @@ $url = "https://api.dorm.com.ng/loginapi.php?phone=".$u."&pass=".$p;
 	if($result->response_code==200){
 Echo '<script type="text/Javascript">window.location.href ="https://app.dorm.com.ng/main.php";</script>';
 	}else{
-		echo'erbsvjkbdfbxv' $result->response_code;
+		echo'erbsvjkbdfbxv'. $result->response_code;
 	}
     
 	?>
