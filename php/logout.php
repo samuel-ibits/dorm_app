0e7593ae0ce8d824f6b0325a9b8565d1f14b0929
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-02 17:37:01 +0100

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -5,7 +5,7 @@ setcookie("dormuserid", "", time() + (86400 * 30), "/");
 setcookie("dormpage", "", time() + (86400 * 30), "/");
 
 setcookie("dormtoken", "", time() + (86400 * 30), "/");
-if($_COOKIE["dormpage"]==""){
-Echo '<script type="text/Javascript">window.location.href ="https://app.dorm.com.ng";</script>';
+if($_COOKIE["dormpage"]=="" and $_COOKIE["dormtoken"]=="" and $_COOKIE["dormuserid"]==""){
+Echo '<script type="text/Javascript">window.location.href ="https://app.dorm.com.ng/index.php";</script>';
 }
 ?>
