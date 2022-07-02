1c6233335de05e70e1c71e18443a491abb002e57
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-02 17:29:51 +0100

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -5,6 +5,7 @@ setcookie("dormuserid", "", time() + (86400 * 30), "/");
 setcookie("dormpage", "", time() + (86400 * 30), "/");
 
 setcookie("dormtoken", "", time() + (86400 * 30), "/");
-
-
+if($_COOKIE["dormpage"]==""){
+Echo '<script type="text/Javascript">window.location.href ="https://app.dorm.com.ng/studytools.php";</script>';
+}
 ?>
