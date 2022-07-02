e8ea5526956dd32b95132b156b623365899154f2
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-02 17:14:59 +0100

    update

---
+++ php/logout.php
@@ -0,0 +1,9 @@
+<?php
+
+setcookie("dormuserid", "", time() + (86400 * 30), "/");
+
+setcookie("dormpage", "", time() + (86400 * 30), "/");
+
+setcookie("dormtoken", "", time() + (86400 * 30), "/");
+
+?>
