af8ce39ae894e3d8f8a2c9c63fc24843d1239f84
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-02 15:36:55 +0100

    Update autologin.php

--- php/autologin.php
+++ php/autologin.php
@@ -1,8 +1,9 @@
 <?php 
 $cookie_name1="dormuserid";
 $cookie_name2="dormtoken";
-$page=$_COOKIE[$cookie_name2];
+$page=$_COOKIE["dormpage"];
 if($_COOKIE[$cookie_name1]!=="" and $_COOKIE[$cookie_name2]!==""){
     header("Location: ".$page);
+    echo $page;
 }
 ?>
