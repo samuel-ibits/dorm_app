b1061b037a8e42d28a9897e177d309f06444c835
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-03 19:56:06 +0100

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,5 +1,7 @@
 <?php include 'getter.php';
 echo $_COOKIE['dormtoken'];
+echo $_COOKIE['dormuserid'];
+echo $_COOKIE['dormpage'];
 
 echo getter('https://api.dorm.com.ng/profilefetch.php');
 	echo'heloo';
