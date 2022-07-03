cae8a3c4c489e0591e9f2ceafd1dc4b1dc201118
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-07-03 17:44:26 +0100

    update

--- php/profile.php
+++ php/profile.php
@@ -1,13 +1,6 @@
-<?php
+<?php include 'getter.php';
 
-$url = "https://api.dorm.com.ng/profilefetch.php";
+
+echo getter('https://api.dorm.com.ng/profilefetch.php');
 	
-	$client = curl_init($url);
-	curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-	$response = curl_exec($client);
-	
-	$result = json_decode($response);
-	echo'test'. $_COOKIE['dormtoken'].$_COOKIE['dormpage'].$_COOKIE['dormuserid'];
-	echo"sdds".$result->phone;
-  
 	?>
